c7de6fa9e3ba30d85a231afd56ae8e9cf8c438d6
superbas11 <bas.schoenmaeckers@gmail.com>
2015-03-28 22:32:38 +0100

    bump up version

--- src/main/java/com/superbas11/MineFX/reference/Reference.java
+++ src/main/java/com/superbas11/MineFX/reference/Reference.java
@@ -3,7 +3,7 @@ package com.superbas11.MineFX.reference;
 public class Reference {
 	public static final String MOD_ID = "MineFX";
 	public static final String MOD_NAME = "MineFX";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.2";
 	public static final String GUI_FACTORY_CLASS = "com.superbas11.MineFX.gui.GuiFactory";
 	public static final String proxy_exe_Download = "https://github.com/superbas11/MineFX/blob/master/proxy%20exe/MineFX.exe?raw=true";
 	public static final String Proxy_dll_Download = "https://github.com/superbas11/MineFX/blob/master/proxy%20exe/Newtonsoft.Json.dll?raw=true";
